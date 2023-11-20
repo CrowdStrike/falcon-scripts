90ba56259924e975bc849ea7fe1c25996ea846ca
Carlos Matos <carlos.matos@crowdstrike.com>
2023-11-20 12:14:38 -0500

    bump v1.1.8 (#223)

--- powershell/migrate/falcon_windows_migrate.ps1
+++ powershell/migrate/falcon_windows_migrate.ps1
@@ -946,7 +946,7 @@ function Get-FalconCloud ([string] $xCsRegion) {
 
 function Invoke-FalconAuth([string] $BaseUrl, [hashtable] $Body, [string] $FalconCloud) {
     $Headers = @{'Accept' = 'application/json'; 'Content-Type' = 'application/x-www-form-urlencoded'; 'charset' = 'utf-8' }
-    $Headers.Add('User-Agent', 'crowdstrike-falcon-scripts/1.1.7')
+    $Headers.Add('User-Agent', 'crowdstrike-falcon-scripts/1.1.8')
     try {
         $response = Invoke-WebRequest -Uri "$($BaseUrl)/oauth2/token" -UseBasicParsing -Method 'POST' -Headers $Headers -Body $Body
         $content = ConvertFrom-Json -InputObject $response.Content
