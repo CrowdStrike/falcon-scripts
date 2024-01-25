408c6be13951202d78c0815b6a4102412c9b1188
Carlos Matos <carlos.matos@crowdstrike.com>
2024-01-25 16:32:06 -0500

    chore: bump v1.2.0

--- bash/install/falcon-linux-install.sh
+++ bash/install/falcon-linux-install.sh
@@ -675,7 +675,7 @@ cs_falcon_oauth_token=$(
     token_result=$(echo "client_id=$cs_falcon_client_id&client_secret=$cs_falcon_client_secret" |
         curl -X POST -s -x "$proxy" -L "https://$(cs_cloud)/oauth2/token" \
             -H 'Content-Type: application/x-www-form-urlencoded; charset=utf-8' \
-            -H 'User-Agent: crowdstrike-falcon-scripts/1.1.9' \
+            -H 'User-Agent: crowdstrike-falcon-scripts/1.2.0' \
             --dump-header "${response_headers}" \
             --data @-)
 
