dc632f643a289e3164a7831c62fc0a6a477653fb
Carlos Matos <carlos.matos@crowdstrike.com>
2024-03-22 10:25:58 -0400

    chore: fix shfmt error

--- bash/install/falcon-linux-install.sh
+++ bash/install/falcon-linux-install.sh
@@ -271,7 +271,7 @@ os_install_package() {
         Ubuntu)
             # If this is ubuntu 14, we need to use dpkg instead
             if [ "${cs_os_version}" -eq 14 ]; then
-                DEBIAN_FRONTEND=noninteractive dpkg -i "$pkg" > /dev/null 2>&1 || true # ignore dep errors
+                DEBIAN_FRONTEND=noninteractive dpkg -i "$pkg" >/dev/null 2>&1 || true # ignore dep errors
                 DEBIAN_FRONTEND=noninteractive apt-get -qq install -f -y >/dev/null
             else
                 DEBIAN_FRONTEND=noninteractive apt-get -qq install -y "$pkg" >/dev/null
